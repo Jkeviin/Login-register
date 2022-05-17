a97df243f3b9a8a985f538fae8170ac1e8fa6c66
Kevin <slash2130kevin@gmail.com>
2022-05-16 21:24:27 -0500

    Create: primera version de formulario sin conexion

---
+++ Assets/JS/Script.js
@@ -0,0 +1,9 @@
+//Declaracion de variables
+
+let contenedor_login_register = document.querySelector(".contenedor__login-register");
+let formulario_Login = document.querySelector(".formulario__login");
+let formulario_register = document.querySelector(".formulario__register");
+
+function register() {
+
+}
